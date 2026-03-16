fa258a68d1294b997fd997c4e7bae1b749da4fb7
zhouxiaofeng <563433585@qq.com>
2026-03-16 22:52:21 +0800

    same up

---
+++ Assets/streamingAssets/PathConfig.lua
@@ -0,0 +1,16 @@
+PathConfig = {
+ CommonPath = "Usedata/Common",
+ UiTexPath = "Usedata/ui/Tex",
+ UiAnimPath = "Usedata/ui/Anim",
+ UiLuaPath = "Usedata/ui/UiLua",
+ GmTexPath = "Usedata/gm/Tex",
+ GmAnimPath = "Usedata/gm/Anim",
+ GmLuaPath = "Usedata/gm/UiLua",
+AddLuaPath = {
+ GmSkillPath = "Usedata/gm/UiLua/Skill",
+ GmAmmoPath = "Usedata/gm/UiLua/Ammo",
+ GmConfigPath = "Usedata/gm/UiLua/Config",
+ GmBsBuffPath = "Usedata/gm/UiLua/BsBuff",
+},
+}
+return PathConfig
